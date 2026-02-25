80866083eb2c1b1c42a466a64eae5a82db510e52
Sparshisthebest <sparsh.nayak0812@gmail.com>
2026-02-25 18:13:38 +0000

    Add files via upload

---
+++ activity1_30.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+class Absclass(ABC):
+    def print(self,x):
+        print("passed value: " , x)
+    def task(self):
+        print("We are in absClass task")
+class test_class(Absclass):
+    def task(self):
+        print("we are inside test_class task")
+test_obj=test_class()
+test_obj.task()
+test_obj.print(100)
+
+
+
+
+
+
+
+
+
